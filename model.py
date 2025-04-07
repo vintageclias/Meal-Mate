2802a0c77c45a9d8d2047d10c7fc804927ec6933
Nzile22 <diananduku00@gmail.com>
2025-04-07 22:06:56 +0300

    added some models

---
+++ model.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.model): #using this for authentication 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False) 
+    password = db.Column(db.String(100), nullable=False)
+    recipes = db.relationship('Recipe', backref='author', lazy=True)  # One-to-many relationship: one user can have many recipes
+    meals = db.relationship('Meal', backref='author', lazy=True) # One-to-many relationship: one user can have many meal plans
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # a foreign key pointing to the User's id (each recipe belongs to a user)
+
+    
+ # Meal Plan model with many-to-many relationship (users and recipes)
+class Meal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    
